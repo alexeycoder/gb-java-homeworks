6fb923971d67b2a699f1786a4f34b520769eb454
Alexey Kuznetsov <leoxander@yandex.ru>
2022-12-04 14:40:31 +0300

    Added alternate merge sort implementation utilizing a pre-allocated buffer.

--- src/main/java/edu/alexey/homework2/ArrayUtils.java
+++ src/main/java/edu/alexey/homework2/ArrayUtils.java
@@ -1,27 +1,91 @@
 package edu.alexey.homework2;
 
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Random;
 
 import edu.alexey.utils.Pair;
 
 public class ArrayUtils {
+
 	// methods
 
-	public static <T extends Comparable<T>> T[] mergeSort(T[] array) {
+	public static <T extends Comparable<T>> void mergeSortBuffered(T[] array) {
+		if (array == null || array.length < 2)
+			return;
+
+		@SuppressWarnings("unchecked")
+		T[] buffer = (T[]) Array.newInstance(array.getClass().getComponentType(), array.length);
+
+		mergeSortBuffered(array, buffer, 0, array.length);
+	}
+
+	private static <T extends Comparable<T>> void mergeSortBuffered(T[] array, T[] buffer, int startInclusive,
+			int endExclusive) {
+		assert endExclusive <= array.length;
+		assert endExclusive > startInclusive;
+
+		int len = endExclusive - startInclusive;
+		assert len > 0;
+		if (len == 1) {
+			return;
+		}
+
+		int rightStartIndex = startInclusive + len / 2; // для нечётных len - правая половина будет на элементик шире
+		mergeSortBuffered(array, buffer, startInclusive, rightStartIndex);
+		mergeSortBuffered(array, buffer, rightStartIndex, endExclusive);
+		mergeSortedPartsBuffered(array, buffer, startInclusive, rightStartIndex, rightStartIndex, endExclusive);
+	}
+
+	private static <T extends Comparable<T>> void mergeSortedPartsBuffered(
+			T[] array, T[] buffer, int leftStartIncl, int leftEndExcl, int rightStartIncl, int rightEndExcl) {
+		assert leftEndExcl > leftStartIncl && rightEndExcl > rightStartIncl;
+		int l = leftStartIncl;
+		int r = rightStartIncl;
+		int i = leftStartIncl;
+
+		// слияние по одному элементу из array в buffer, пока какой-нибудь из массивов
+		// не закончится:
+		while (l < leftEndExcl && r < rightEndExcl) {
+			var leftVal = array[l];
+			var rightVal = array[r];
+			if (rightVal == null || rightVal.compareTo(leftVal) < 0) {
+				buffer[i] = rightVal;
+				++r;
+
+			} else {
+				buffer[i] = leftVal;
+				++l;
+			}
+			++i;
+		}
+		// добавляем в конец buffer'а оставшийся хвостик левого массива,
+		// если таковой имеется:
+		for (; l < leftEndExcl; ++l, ++i) {
+			buffer[i] = array[l];
+		}
+		// возможный хвостик правого не имеет смысла копировать в buffer, поскольку
+		// эти элементы в последствии также должны оказаться в исходном массиве,
+		// где они итак уже имеются.
+
+		// копируем сформированную 'рабочую' часть буфера, содержащую слитые элементы,
+		// в исходный массив:
+		for (--i; i >= leftStartIncl; --i) {
+			array[i] = buffer[i];
+		}
+	}
+
+	public static <T extends Comparable<T>> void mergeSort(T[] array) {
 		int len = array.length;
 		if (len <= 1) {
-			return array;
+			return;
 		}
 
 		var pair = splitArray(array);
 
-		var leftSorted = mergeSort(pair.left());
-		var rightSorted = mergeSort(pair.right());
-
-		mergeArrays(array, leftSorted, rightSorted);
-
-		return array;
+		mergeSort(pair.left());
+		mergeSort(pair.right());
+		mergeArrays(array, pair.left(), pair.right());
 	}
 
 	private static <T> Pair<T[]> splitArray(T[] array) {
@@ -39,7 +103,7 @@ public class ArrayUtils {
 		int leftIndex = 0;
 		int rightIndex = 0;
 		int i = 0;
-		// сливаем по одному элементу, пока какой-нибудь из массивов не закончится:
+		// слияние по одному элементу, пока какой-нибудь из массивов не закончится:
 		while (leftIndex < left.length && rightIndex < right.length) {
 			var leftVal = left[leftIndex];
 			var rightVal = right[rightIndex];
